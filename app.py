341c7143ccbeadad4729dd4345c8911fb0392027
salesforce2019-GitHub <48716773+salesforce2019-GitHub@users.noreply.github.com>
2020-03-09 19:01:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server - ashish !!</h1>"
-
+    ### return "<h1>Welcome to our server - ashish !!</h1>"
+ user = {'username': 'Miguel'}
+    ## return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
