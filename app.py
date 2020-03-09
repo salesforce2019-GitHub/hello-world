93066fbf0f1b2b6a23a3d934a7bc908a746bbda8
salesforce2019-GitHub <48716773+salesforce2019-GitHub@users.noreply.github.com>
2020-03-09 18:57:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
-@app.route('/index/', methods=['POST'])
+@app.route('/')
 def index():
-   return "Hello, World!"
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
