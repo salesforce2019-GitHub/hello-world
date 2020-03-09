5eef8cef66a26ba791b54df519050836c1be1a13
salesforce2019-GitHub <48716773+salesforce2019-GitHub@users.noreply.github.com>
2020-03-09 18:49:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    ## return render_template('index.html', title='Home', user=user)
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+   return "Hello, World!"
